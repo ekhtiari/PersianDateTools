e8a21a5bbd5c39dd7371b9c01c8e31be45f04a3d
Iman Ekhtiari <i.ekhtiari@gmail.com>
2021-02-04 09:15:21 +0330

    Add project files.

---
+++ PersianDateTools/DateTools.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersianDateTools
+{
+    public static class DateTools
+    {
+        public static int CalculateAge(DateTime birthdate)
+        {
+            // Save today's date.
+            var today = DateTime.Today;
+            // Calculate the age.
+            var age = today.Year - birthdate.Year;
+            // Go back to the year the person was born in case of a leap year
+            if (birthdate > today.AddYears(-age)) age--;
+
+            return age;
+        }
+
+        public static string CalculateBirthDay(DateTime? birthday)
+        {
+            if (!birthday.HasValue)
+            {
+                return $"0 سال 0 ماه";
+            }
+
+            var today = DateTime.Today;
+
+            var months = today.Month - birthday.Value.Month;
+            var years = today.Year - birthday.Value.Year;
+
+            if (today.Day < birthday.Value.Day)
+            {
+                months--;
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+
+
+            return
+                $"{years} سال, {months} ماه";
+        }
+
+        public static string Miladi2Shamsi(string mydate)
+        {
+            try
+            {
+                var _date = DateTime.Parse(mydate);
+                PersianCalendar pc = new PersianCalendar();
+                StringBuilder sb = new StringBuilder();
+                sb.Append(pc.GetYear(_date).ToString("0000"));
+                sb.Append("/");
+                sb.Append(pc.GetMonth(_date).ToString("00"));
+                sb.Append("/");
+                sb.Append(pc.GetDayOfMonth(_date).ToString("00"));
+                return sb.ToString();
+            }
+            catch 
+            {
+                return "0000/00/00";
+                
+            }
+           
+        }
+
+        public static DateTime Shamsi2Miladi(string _date)
+        {
+            int year = int.Parse(_date.Substring(0, 4));
+            int month = int.Parse(_date.Substring(5, 2));
+            int day = int.Parse(_date.Substring(8, 2));
+            PersianCalendar p = new PersianCalendar();
+            DateTime date = p.ToDateTime(year, month, day, 0, 0, 0, 0);
+            return date;
+        }
+
+        public static string DayName(DateTime d)
+        {
+            var dname = d.DayOfWeek;
+            switch (dname)
+            {
+                case DayOfWeek.Sunday:
+                    return "یکشنبه";
+                    
+                case DayOfWeek.Monday:
+                    return "دوشنبه";
+                case DayOfWeek.Tuesday:
+                    return "سه شنبه";
+                case DayOfWeek.Wednesday:
+                    return "چهارشنبه";
+                case DayOfWeek.Thursday:
+                    return "پنجشنبه";
+                case DayOfWeek.Friday:
+                    return "جمعه";
+                case DayOfWeek.Saturday:
+                    return "شنبه";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ PersianDateTools/PersianCulture.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PersianDateTools
+{
+    public static class PersianDateExtensionMethods
+    {
+        private static CultureInfo _Culture;
+        public static CultureInfo GetPersianCulture()
+        {
+            if (_Culture == null)
+            {
+                _Culture = new CultureInfo("fa-IR");
+                DateTimeFormatInfo formatInfo = _Culture.DateTimeFormat;
+                formatInfo.AbbreviatedDayNames = new[] { "ی", "د", "س", "چ", "پ", "ج", "ش" };
+                //formatInfo.DayNames = new[] { "یکشنبه", "دوشنبه", "سه شنبه", "چهار شنبه", "پنجشنبه", "جمعه", "شنبه" };
+                formatInfo.DayNames = new[] { "#1", "#2", "#3", "#4", "#5", "#6", "#0" };
+                var monthNames = new[]
+                {
+                    "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن",
+                    "اسفند",
+                    ""
+                };
+                formatInfo.AbbreviatedMonthNames =
+                    formatInfo.MonthNames =
+                    formatInfo.MonthGenitiveNames = formatInfo.AbbreviatedMonthGenitiveNames = monthNames;
+                formatInfo.AMDesignator = "ق.ظ";
+                formatInfo.PMDesignator = "ب.ظ";
+                formatInfo.ShortDatePattern = "yyyy/MM/dd";
+                formatInfo.LongDatePattern = "dddd, dd MMMM,yyyy";
+                formatInfo.FirstDayOfWeek = DayOfWeek.Saturday;
+                System.Globalization.Calendar cal = new PersianCalendar();
+
+                FieldInfo fieldInfo = _Culture.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (fieldInfo != null)
+                    fieldInfo.SetValue(_Culture, cal);
+
+                FieldInfo info = formatInfo.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (info != null)
+                    info.SetValue(formatInfo, cal);
+
+                _Culture.NumberFormat.NumberDecimalSeparator = "/";
+                _Culture.NumberFormat.DigitSubstitution = DigitShapes.NativeNational;
+                _Culture.NumberFormat.NumberNegativePattern = 0;
+            }
+            return _Culture;
+        }
+
+        public static string ToPeString(this DateTime date, string format = "yyyy/MM/dd")
+        {
+            return date.ToString(format, GetPersianCulture());
+        }
+    }
+
+    public static class getDateDetail
+    {
+        public static List<string> getDayOfThisMonth(DateTime _date)
+        {
+            List<string> ListDay = new List<string>();
+
+            //Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = PersianDateExtensionMethods.GetPersianCulture();
+
+            DateTime GetDate = _date;
+            string month = GetDate.ToPeString("MM");
+            bool firstDay = true;
+
+            for (int i = 31; i > 0; i--)
+            {
+
+                if (GetDate.AddDays(-i).ToPeString("MM") == month)
+                {
+                    if (firstDay)
+                    {
+                        ListDay.Add(GetDate.AddDays(-i).ToPeString("dddd") + GetDate.AddDays(-i).ToPeString("dd"));
+                        firstDay = false;
+                    }
+                    else
+                    {
+                        ListDay.Add(GetDate.AddDays(-i).ToPeString("dd"));
+                    }
+
+                }
+
+            }
+
+
+
+            if (firstDay)
+            {
+                ListDay.Add(GetDate.ToPeString("dddd") + GetDate.ToPeString("dd"));
+                firstDay = false;
+            }
+            else
+            {
+                ListDay.Add("-" + GetDate.ToPeString("dd"));
+            }
+
+            for (int i = 1; i < 31; i++)
+            {
+
+                if (GetDate.AddDays(i).ToPeString("MM") == month)
+                {
+                    ListDay.Add(GetDate.AddDays(i).ToPeString("dd"));
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+
+
+            return ListDay;
+        }
+    }
+}
